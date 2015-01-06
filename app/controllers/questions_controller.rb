32a0b554c03ce00c4b74985c91da2e6443f6a04b
tina albores <tina@clinic-it.com>
2015-01-06 14:30:26 +0800

    added edit/update actions (with view)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,14 +7,34 @@ class QuestionsController < ApplicationController
   
 
   def new
+    @question = Question.new
+  end
+
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   
   def create
     @question = Question.new(question_params)
 
-    @question.save
-    redirect_to @question
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
   end
 
 
