1771a8dfc7457304c92a59b01ee2d5eb38c66fd5
tina albores <tina@clinic-it.com>
2015-01-06 16:15:20 +0800

    generated answer model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ActiveRecord::Base
 
+  has_many :answers
+  
+  
   validates :title, presence: true
   validates :text, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106035143) do
+ActiveRecord::Schema.define(version: 20150106073148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20150106035143) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
