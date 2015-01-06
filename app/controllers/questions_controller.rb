9d6a0f9944ab27bd971a598c14008178a7f758ac
tina albores <tina@clinic-it.com>
2015-01-06 13:27:00 +0800

    new actions for question controller (with views): new, index, show, create (redirects to show)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,34 @@
 class QuestionsController < ApplicationController
+
+  
+  def index
+    @questions = Question.all
+  end
+  
+
   def new
+  end
+
+  
+  def create
+    @question = Question.new(question_params)
+
+    @question.save
+    redirect_to @question
+  end
+
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+
+
+
+  
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :text)
   end
 end
