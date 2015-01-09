929c7fefaa35cb4eb73cc8678a01db38ef996b1c
tina albores <tina@clinic-it.com>
2015-01-09 12:06:13 +0800

    generated mailer models, added template for mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@soclone.com"
   layout 'mailer'
 end
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,2 +1,13 @@
 class Notifier < ApplicationMailer
+  
+  def forgot_password to, pass, sent_at = Time.now
+    @pass = pass
+    mail(
+         :subject => 'SoClone - Forgot password',
+         :to => to,
+         :from => 'no-reply@soclone.com',
+         :date => sent_at
+         )
+  end
+  
 end
