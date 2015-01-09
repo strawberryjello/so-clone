eaaeb5e80ddb65a668fce4f73e4a8dfa1bc7012a
tina albores <tina@clinic-it.com>
2015-01-09 19:21:26 +0800

    added tag text field to create question view, added transient tag_string field to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,12 @@
 class Question < ActiveRecord::Base
 
+  attr_accessor :tag_string
+  
+
   belongs_to :user
 
   has_many :answers, dependent: :destroy
+  has_and_belongs_to_many :tags
   
   
   validates_presence_of :title, :text
