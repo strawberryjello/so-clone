ce3d0281207d7ebf23d8431b0fad153ec25ebe65
tina albores <tina@clinic-it.com>
2015-01-13 12:14:23 +0800

    added whitespace stripping prior to saving tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,6 +11,7 @@ class Tag < ActiveRecord::Base
     tags = []
     tag_strings = tag_string.downcase.split ','
     tag_strings.each do |t|
+      t.strip!
       old_tag = find_by(:name => t)
       if old_tag
         tags << old_tag
