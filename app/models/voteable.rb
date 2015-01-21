1767ee8772c7c085e84196cad834a5464385053c
tina albores <tina@clinic-it.com>
2015-01-21 18:24:43 +0800

    removed previous implementation of voteable, replaced with a vote tally method

--- app/models/voteable.rb
+++ app/models/voteable.rb
@@ -1,16 +1,7 @@
 module Voteable
-
-  def init_votes
-    self.votes = 0
-  end
-
   
-  def upvote
-    self.votes += 1
+  def votes
+    return self.upvotes.size - self.downvotes.size
   end
 
-  
-  def downvote
-    self.votes -= 1
-  end
 end
