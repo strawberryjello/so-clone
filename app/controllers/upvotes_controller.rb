60344ca6e9decf5224e27e79c0bfa111691ec71b
tina albores <tina@clinic-it.com>
2015-01-21 18:19:51 +0800

    generated upvote controller, filled out its create method

---
+++ app/controllers/upvotes_controller.rb
@@ -0,0 +1,21 @@
+class UpvotesController < ApplicationController
+
+  before_action :login_required
+
+
+  def create
+    @question = Question.find params[:question_id]
+    @user = User.find session[:user_id]
+
+    if Upvote.exists?(:voteable_id => @question.id, :user_id => @user.id)
+      flash[:error] = 'You have already upvoted this question'
+    else
+      flash[:message] = 'Question upvoted'
+      @upvote = @question.upvotes.create
+      @upvote.user = @user
+      @upvote.save
+    end
+    
+    redirect_to question_path @question
+  end
+end
