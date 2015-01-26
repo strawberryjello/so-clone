13426f6ea67e545902ad61f9668da1a23237e3ff
tina albores <tina@clinic-it.com>
2015-01-26 16:30:56 +0800

    updated model validation tests for question and tag

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,19 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question do
-  context 'validations' do
-    it 'should check presence of title' do
-      question = Question.new(:text => 'text')
-      expect(question.save).to be false
-    end
-    
-    it 'should check presence of text' do
-      question = Question.new(:title => 'title')
-      expect(question.save).to be false
-    end
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:text) }
   end
   
-  context 'tagging' do
+  describe 'tagging' do
     it 'should add new tags to a question' do
       t1 = Tag.new(:name => 't1')
       question = Question.new
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Tag do
-  context 'parsing' do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+  
+  describe 'parsing' do
     it 'should create new tags' do
       str = 'single'
       
