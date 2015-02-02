9c0352791d8d4955fab326edb8dc99c00a614c3b
tina albores <tina@clinic-it.com>
2015-02-02 16:30:59 +0800

    added cancel method to upvote/downvote models

--- app/models/downvote.rb
+++ app/models/downvote.rb
@@ -3,4 +3,10 @@ class Downvote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
   belongs_to :user
   
+  
+  def self.cancel question_id, user_id
+    downvote = Downvote.find_by(:voteable_id => question_id, :user_id => user_id)
+    downvote.destroy
+  end
+  
 end
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -2,5 +2,11 @@ class Upvote < ActiveRecord::Base
 
   belongs_to :voteable, polymorphic: true
   belongs_to :user
+
+
+  def self.cancel question_id, user_id
+    upvote = Upvote.find_by(:voteable_id => question_id, :user_id => user_id)
+    upvote.destroy
+  end
   
 end
