fa6679a6d00c08beb6bb0aca81f470ab5faab3cd
tina albores <tina@clinicny.com>
2016-01-27 18:58:57 +0800

    Remove extra whitespace

--- app/models/downvote.rb
+++ app/models/downvote.rb
@@ -2,11 +2,11 @@ class Downvote < ActiveRecord::Base
 
   belongs_to :voteable, polymorphic: true
   belongs_to :user
-  
-  
+
+
   def self.cancel voteable_id, user_id
     downvote = Downvote.find_by(:voteable_id => voteable_id, :user_id => user_id)
     downvote.destroy
   end
-  
+
 end
