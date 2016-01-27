efb44ac6271a8b6697540f354df4a31d65d1291a
tina albores <tina@clinicny.com>
2016-01-27 14:21:31 +0800

    Remove extra whitespace

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -2,7 +2,7 @@ class DownvotesController < ApplicationController
 
   before_action :login_required
 
-  
+
   def downvote_question
     @question = Question.find params[:question_id]
     @user = User.find session[:user_id]
@@ -11,12 +11,12 @@ class DownvotesController < ApplicationController
       Upvote.cancel(@question.id, @user.id)
 
       resolve_update @question.votes, @question
-      
+
     elsif Downvote.exists?(:voteable_id => @question.id, :user_id => @user.id)
       error_msg = 'You already downvoted this question'
-      
+
       resolve_duplicate error_msg
-      
+
     else
       @downvote = @question.downvotes.create
       @downvote.user = @user
@@ -24,9 +24,8 @@ class DownvotesController < ApplicationController
 
       resolve_update @question.votes, @question
     end
-    
   end
-  
+
   def downvote_answer
     @question = Question.find params[:question_id]
     @answer = Answer.find params[:answer_id]
@@ -36,12 +35,12 @@ class DownvotesController < ApplicationController
       Upvote.cancel(@answer.id, @user.id)
 
       resolve_update @answer.votes, @question
-      
+
     elsif Downvote.exists?(:voteable_id => @answer.id, :user_id => @user.id)
       error_msg = 'You already downvoted that answer'
 
       resolve_duplicate error_msg
-      
+
     else
       @downvote = @answer.downvotes.create
       @downvote.user = @user
@@ -49,7 +48,6 @@ class DownvotesController < ApplicationController
 
       resolve_update @answer.votes, @question
     end
-    
   end
 
 
