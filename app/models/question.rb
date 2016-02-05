8c4b1162e64e730ffc804f4cf6f5f48d0a402f53
tina albores <tina@clinicny.com>
2016-02-05 14:28:40 +0800

    Remove extra whitespace

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,21 +2,21 @@ class Question < ActiveRecord::Base
 
   include Voteable
   include Mentionable
-  
+
 
   attr_accessor :tag_string
-  
+
 
   belongs_to :user
 
   has_many :answers, dependent: :destroy
-  
+
   has_many :upvotes, as: :voteable
   has_many :downvotes, as: :voteable
-  
+
   has_and_belongs_to_many :tags
-  
-  
+
+
   validates_presence_of :title, :text
 
 
